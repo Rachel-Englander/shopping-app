fad7d8114f5a8375dbdd06b7c886e27f2e79dade
Rachel-Englander <m0556771142@gmail.com>
2025-06-25 23:43:06 +0300

    Update vite.config.ts

--- react-order-client/vite.config.ts
+++ react-order-client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 קריטי לפרודקשן - להפוך נתיבים ליחסיים
   server: {
     port: 5173
   }
